cbed1f13bf4bf338da8509b8c75c13f528b5007e
Dalton Dam <daltondam26@gmail.com>
2025-08-26 16:40:00 -0300

    refactor: ResponseEntity and Mapper implementation

--- src/main/java/io/github/dam/raven/controller/GenreController.java
+++ src/main/java/io/github/dam/raven/controller/GenreController.java
@@ -1,8 +1,14 @@
 package io.github.dam.raven.controller;
 
+import io.github.dam.raven.controller.request.GenreRequest;
+import io.github.dam.raven.controller.response.GenreResponse;
 import io.github.dam.raven.entity.Genre;
+import io.github.dam.raven.mapper.GenreMapper;
 import io.github.dam.raven.service.GenreService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,22 +22,26 @@ public class GenreController {
     private final GenreService genreService;
 
     @GetMapping()
-    public List<Genre> getAllGenres() {
-        return genreService.findAll();
+    public ResponseEntity<List<GenreResponse>> findAll() {
+        List<GenreResponse> genres = genreService.findAll().stream()
+                .map(GenreMapper::toGenreResponse)
+                .toList();
+
+        return ResponseEntity.ok(genres);
     }
 
     @PostMapping
-    public Genre saveGenre(@RequestBody Genre genre) {
-        return genreService.saveGenre(genre);
+    public ResponseEntity<GenreResponse> createGenre(@Valid @RequestBody GenreRequest genreRequest) {
+        Genre newGenre = GenreMapper.toGenre(genreRequest);
+        Genre savedGenre = genreService.saveGenre(newGenre);
+        return ResponseEntity.status(HttpStatus.CREATED).body(GenreMapper.toGenreResponse(newGenre));
     }
 
     @GetMapping("/{id}")
-    public Genre getByGenreId(@PathVariable Long id) {
-        Optional<Genre> optGenre = genreService.findById(id);
-        if(optGenre.isPresent()) {
-            return optGenre.get();
-        }
-        return null;
+    public ResponseEntity<GenreResponse> find(@PathVariable Long id) {
+        return genreService.findById(id)
+                .map(genre -> ResponseEntity.ok(GenreMapper.toGenreResponse(genre)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
